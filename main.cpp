4e4df5bb11c7184c12a91d501061decd89ae6a52
Harry Steiner <harrysteiner@gmx.net>
2016-03-30 22:10:50 +0200

    initially added

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: juia
+ *
+ * Created on 30. März 2016, 21:58
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+  return 0;
+}
+
